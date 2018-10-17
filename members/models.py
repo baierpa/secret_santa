560c0d33bbc72d8bbc2d69c348fef41de48e1e4c
baierpa <baierpa@g.cofc.edu>
2018-10-16 22:53:02 -0400

    add fields to members db

--- members/models.py
+++ members/models.py
@@ -3,6 +3,10 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class Members(AbstractUser):
+    street = models.CharField(max_length=30, blank=True, null=True)
+    state = models.CharField(max_length=2, blank=True, null=True)
+    zip_code = models.CharField(max_length=5, blank=True, null=True)
+    phone = models.CharField(max_length=10, blank=True, null=True)
 
     def __str__(self):
         return self.pk
