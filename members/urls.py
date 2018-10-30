940fd45efebcf584b9ef8632f4b7aefa6f216eb2
baierpa <baierpa@g.cofc.edu>
2018-10-29 23:02:05 -0400

    move login to members app

--- members/urls.py
+++ members/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
-from .views import SignUpView
+from .views import MembersView
 
 app_name = 'members'
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
-    path('signup/', SignUpView.as_view(), name='signup')
+    path('signup/', MembersView.as_view(), name='signup')
 ]
--- members/views.py
+++ members/views.py
@@ -2,7 +2,7 @@ from members.forms import MembersCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 
-class SignUpView(generic.CreateView):
+class MembersView(generic.CreateView):
     form_class = MembersCreationForm
     success_url = reverse_lazy('home')
     template_name = 'signup.html'
